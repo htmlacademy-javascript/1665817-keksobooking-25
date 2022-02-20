f9df62b05af5fd10fc9e5ab1e7cb038e2ea08370
Kromus22 <kromus2@gmail.com>
2022-02-20 16:21:00 +0500

    возвращение блудного return'a + успокоение ESLint

--- js/main.js
+++ js/main.js
@@ -1,22 +1,23 @@
 const getRandomNumber = (min, max) => {
   if (min >= max || min < 0) {
-    console.log('Дружище, перепроверь числа, которые пытаешься запихать сюда.');
+    throw new Error('Дружище, перепроверь числа, которые пытаешься запихать сюда.');
   }
-  Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
 };
 getRandomNumber();
 
 
-const coordinate = (min, max, numder) => {
+const getRandomFloat = (min, max, numder) => {
   if (min >= max || min < 0) {
-    console.log('Дружище, перепроверь числа, которые пытаешься запихать сюда.');
+    throw new Error('Дружище, перепроверь числа, которые пытаешься запихать сюда.');
   }
   const fraction = Math.random() * (max - min) + min;
 
   return fraction.toFixed(numder);
 };
 
-coordinate();
+getRandomFloat();
 
 // https://codepen.io/jsundai/pen/vYKxNZp
 // https://w3schoolsrus.github.io/js/js_number_methods.html#gsc.tab=0
