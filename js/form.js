8a477f6dca22fc87f4b85981a8a45b8923336bdc
Kromus22 <kromus2@gmail.com>
2022-03-11 16:54:19 +0500

    domashka

--- js/form.js
+++ js/form.js
@@ -2,6 +2,7 @@ const form = document.querySelector('.ad-form');
 const formInputs = form.querySelectorAll('fieldset');
 const mapFilters = document.querySelector('.map__filters');
 const mapInputs = mapFilters.querySelectorAll('select, fieldset');
+const map = document.querySelector('.map__canvas');
 
 form.classList.add('ad-form--disabled');
 mapFilters.classList.add('ad-form--disabled');
@@ -14,3 +15,17 @@ for (let i = 0; i < mapInputs.length; i++) {
   mapInputs[i].disabled = true;
 }
 
+const unblockForms = () => {
+  form.classList.remove('ad-form--disabled');
+  mapFilters.classList.remove('ad-form--disabled');
+
+  for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].disabled = false;
+  }
+
+  for (let i = 0; i < mapInputs.length; i++) {
+    mapInputs[i].disabled = false;
+  }
+};
+
+export { unblockForms, map };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 import { createAd } from './data.js';
 import { render } from './renderData.js';
+import { unblockForms, map } from './form.js';
 
 const ads = Array.from({ length: 1 }, createAd);
 render(ads);
 
+//просто, как вариант разблокировки форм, чтобы выполнялось задание.
+map.addEventListener('click', () => {
+  unblockForms();
+});
